912a62f9279c097b87874e08fb72b4d38ad2a249
huy.td1 <huy.td1@samsung.com>
2022-03-11 10:18:10 +0700

    [Impl] Implement Hanza TreeView Item patch#2

--- LogGuard_v0.1-master/LogGuard_v0.1/LogGuard/Control/HanzaTreeViewItem.cs
+++ LogGuard_v0.1-master/LogGuard_v0.1/LogGuard/Control/HanzaTreeViewItem.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
@@ -17,6 +18,7 @@ namespace LogGuard_v0._1.LogGuard.Control
     {
         private const string AddBtnName = "PART_addBtn";
         private const string RemoveBtnName = "PART_removeBtn";
+        private const string ExpandBtnName = "Expander";
         public HanzaTreeViewItem()
         {
             this.DefaultStyleKey = typeof(TreeViewItem);
@@ -24,12 +26,15 @@ namespace LogGuard_v0._1.LogGuard.Control
 
         private Button _addButton;
         private Button _removeButton;
+        private ToggleButton _expandButton;
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
             _addButton = GetTemplateChild(AddBtnName) as Button;
             _removeButton = GetTemplateChild(RemoveBtnName) as Button;
-
+            _expandButton = GetTemplateChild(ExpandBtnName) as ToggleButton;
+            var animTime = 100;
+            var mainGrid = GetTemplateChild("MainGrid") as Grid;
             if (_addButton == null || _removeButton == null)
             {
                 throw new InvalidOperationException("Some view elements not found!");
@@ -68,6 +73,69 @@ namespace LogGuard_v0._1.LogGuard.Control
                 parma[0] = this;
                 parma[1] = e;
                 source?.RemoveBtnCommand?.Execute(parma);
+            };
+
+            _expandButton.Checked += (s,e) =>
+            {
+                Storyboard expandSb = new Storyboard();
+
+                DoubleAnimation rotateAnim = new DoubleAnimation();
+                rotateAnim.From = 0.0;
+                rotateAnim.To = 90.0;
+                rotateAnim.Duration = new Duration(TimeSpan.FromMilliseconds(animTime));
+                Storyboard.SetTargetName(rotateAnim, "PART_ToggleButtonRotateTransform");
+                Storyboard.SetTargetProperty(rotateAnim, new PropertyPath(RotateTransform.AngleProperty));
+                expandSb.Children.Add(rotateAnim);
+
+                ObjectAnimationUsingKeyFrames oAUKF = new ObjectAnimationUsingKeyFrames();
+                DiscreteObjectKeyFrame dOKF = new DiscreteObjectKeyFrame();
+                dOKF.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0));
+                dOKF.Value = Visibility.Visible;
+                oAUKF.KeyFrames.Add(dOKF);
+                Storyboard.SetTargetName(oAUKF, "ItemsHost");
+                Storyboard.SetTargetProperty(oAUKF, new PropertyPath(UIElement.VisibilityProperty));
+                expandSb.Children.Add(oAUKF);
+
+                DoubleAnimation openItemHostAnim = new DoubleAnimation();
+                openItemHostAnim.From = 0.0;
+                openItemHostAnim.To = 1.0;
+                openItemHostAnim.Duration = new Duration(TimeSpan.FromMilliseconds(animTime));
+                Storyboard.SetTargetName(openItemHostAnim, "PART_ItemHostScaleTransform");
+                Storyboard.SetTargetProperty(openItemHostAnim, new PropertyPath(ScaleTransform.ScaleYProperty));
+                expandSb.Children.Add(openItemHostAnim);
+
+                expandSb.Begin(mainGrid);
+            };
+            _expandButton.Unchecked += (s, e) =>
+            {
+                Storyboard dispandSB = new Storyboard();
+
+                DoubleAnimation rotateAnim = new DoubleAnimation();
+                rotateAnim.From = 90.0;
+                rotateAnim.To = 0.0;
+                rotateAnim.Duration = new Duration(TimeSpan.FromMilliseconds(animTime));
+                Storyboard.SetTargetName(rotateAnim, "PART_ToggleButtonRotateTransform");
+                Storyboard.SetTargetProperty(rotateAnim, new PropertyPath(RotateTransform.AngleProperty));
+                dispandSB.Children.Add(rotateAnim);
+
+                ObjectAnimationUsingKeyFrames oAUKF = new ObjectAnimationUsingKeyFrames();
+                DiscreteObjectKeyFrame dOKF = new DiscreteObjectKeyFrame();
+                dOKF.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(animTime));
+                dOKF.Value = Visibility.Collapsed;
+                oAUKF.KeyFrames.Add(dOKF);
+                Storyboard.SetTargetName(oAUKF, "ItemsHost");
+                Storyboard.SetTargetProperty(oAUKF, new PropertyPath(UIElement.VisibilityProperty));
+                dispandSB.Children.Add(oAUKF);
+
+                DoubleAnimation openItemHostAnim = new DoubleAnimation();
+                openItemHostAnim.From = 1.0;
+                openItemHostAnim.To = 0.0;
+                openItemHostAnim.Duration = new Duration(TimeSpan.FromMilliseconds(animTime));
+                Storyboard.SetTargetName(openItemHostAnim, "PART_ItemHostScaleTransform");
+                Storyboard.SetTargetProperty(openItemHostAnim, new PropertyPath(ScaleTransform.ScaleYProperty));
+                dispandSB.Children.Add(openItemHostAnim);
+
+                dispandSB.Begin(mainGrid);
             };
         }
 
--- LogGuard_v0.1-master/LogGuard_v0.1/MVVM/View/UserControls/UC_LogManager.xaml.cs
+++ LogGuard_v0.1-master/LogGuard_v0.1/MVVM/View/UserControls/UC_LogManager.xaml.cs
@@ -1,4 +1,6 @@
-﻿using LogGuard_v0._1.MVVM.ViewModels;
+﻿using LogGuard_v0._1.Base.Command;
+using LogGuard_v0._1.LogGuard.Control;
+using LogGuard_v0._1.MVVM.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -22,27 +24,95 @@ namespace LogGuard_v0._1.MVVM.View.UserControls
     /// </summary>
     public partial class UC_LogManager : UserControl
     {
+        public ObservableCollection<MenuItem> Items { get; set; } = new ObservableCollection<MenuItem>();
+
         public UC_LogManager()
         {
             InitializeComponent();
 
             var source = new ObservableCollection<LogByTeamItemViewModel>();
 
-            var perForItem = new LogByTeamItemViewModel("P4");
-            perForItem.AddItem(new LogByTeamItemViewModel("Setting"));
-            perForItem.AddItem(new LogByTeamItemViewModel("Config1"));
-           
+            var perForItem = new LogByTeamItemViewModel(source, "P4");
 
-            var item1 = new LogByTeamItemViewModel("SIP team")
-                .AddItem(perForItem)
-                .AddItem(new LogByTeamItemViewModel("Performance"))
-                .AddItem(new LogByTeamItemViewModel("View"));
+            perForItem.AddItem(new LogByTeamItemViewModel(perForItem, "Setting"));
+            perForItem.AddItem(new LogByTeamItemViewModel(perForItem, "Config"));
+
+            for (int i = 0; i < 1000; i++)
+            {
+                perForItem.AddItem(new LogByTeamItemViewModel(perForItem, "" + i));
+            }
+
+            var item1 = new LogByTeamItemViewModel(source, "SIP team");
+            item1.AddItem(new LogByTeamItemViewModel(item1, "Performance"));
+            item1.AddItem(new LogByTeamItemViewModel(item1, "View"));
 
             source.Add(item1);
-            source.Add(item1);
-            source.Add(item1);
-            source.Add(item1);
+            source.Add(perForItem);
             TeamTreeView.ItemsSource = source;
+
+            //MenuItem root = new MenuItem(Items) { Title = "Menu" };
+            //MenuItem childItem1 = new MenuItem(root) { Title = "Child item #1" };
+            //root.Items.Add(childItem1);
+            //root.Items.Add(new MenuItem(root) { Title = "Child item #2" });
+
+            //Items.Add(root);
+            //TeamTreeView.ItemsSource = Items;
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    MenuItem root2 = new MenuItem(Items) { Title = i + "" };
+            //    Items.Add(root2);
+
+            //}
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    MenuItem root2 = new MenuItem(childItem1) { Title = i + "" };
+            //    childItem1.Items.Add(root2);
+
+            //}
         }
+
+
+    }
+
+    public class MenuItem : IHanzaTreeViewItem
+    {
+        private BaseCommandImpl addCmd;
+        private BaseCommandImpl rmCmd;
+        private object parent;
+        public MenuItem(object par)
+        {
+            this.Items = new ObservableCollection<MenuItem>();
+            this.parent = par;
+            addCmd = new BaseCommandImpl((s, e) =>
+            {
+                int a = 1;
+
+            });
+
+            rmCmd = new BaseCommandImpl((s, e) =>
+            {
+                int a = 1;
+                var hzItem = s as HanzaTreeViewItem;
+                if (parent != null && hzItem != null)
+                {
+                    if (parent is MenuItem)
+                    {
+                        var cast = parent as MenuItem;
+                        cast?.Items.Remove(hzItem.DataContext as MenuItem);
+                    }
+                    else if (parent is ObservableCollection<MenuItem>)
+                    {
+                        var cast = parent as ObservableCollection<MenuItem>;
+                        cast?.Remove(hzItem.DataContext as MenuItem);
+                    }
+                }
+            });
+        }
+        public string Title { get; set; }
+        public ObservableCollection<MenuItem> Items { get; set; }
+        public ICommand AddBtnCommand { get => addCmd; }
+        public ICommand RemoveBtnCommand { get => rmCmd; }
     }
 }
