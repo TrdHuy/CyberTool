2d82015bd5a4ba98f4e742d30e5d4e8084cda8c4
huy.td1 <huy.td1@samsung.com>
2022-03-18 14:48:12 +0700

    [Impl] Implement MenuPanel for MainWindow

---
+++ LogGuard_v0.1-master/LogGuard_v0.1/Utils/Converters/StringToGeoPathConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace LogGuard_v0._1.Utils.Converters
+{
+    public class StringToGeoPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Geometry.Parse(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
