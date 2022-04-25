e0063a59c245453186249d8bdd0b5c8344f7dcfc
huy.td1 <huy.td1@samsung.com>
2022-04-25 15:23:22 +0700

    [Issue] LogGuard window takes full screen when change maximize state (overlap the taskbar)
    M: Listen to msg WM_GETMINMAXINFO to adjust window size, this msg was recieved from win32 whenever screen size changed

--- LogGuard_v0.1-master/LogGuard_v0.1/AppResources/Controls/LogGWindows/LogGuardWindow.cs
+++ LogGuard_v0.1-master/LogGuard_v0.1/AppResources/Controls/LogGWindows/LogGuardWindow.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using LogGuard_v0._1.Utils;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WinInterop = System.Windows.Interop;
+using System.Runtime.InteropServices;
 
 namespace LogGuard_v0._1.AppResources.Controls.LogGWindows
 {
@@ -37,8 +43,14 @@ namespace LogGuard_v0._1.AppResources.Controls.LogGWindows
         public LogGuardWindow()
         {
             DefaultStyleKey = typeof(LogGuardWindow);
-
+            SourceInitialized += new EventHandler((s, e) =>
+            {
+                System.IntPtr handle = (new WinInterop.WindowInteropHelper(this)).Handle;
+                WinInterop.HwndSource.FromHwnd(handle).AddHook(new WinInterop.HwndSourceHook(WindowProc));
+            });
         }
+
+
 
         private Button _minimizeBtn;
         private Button _maximizeBtn;
@@ -76,5 +88,58 @@ namespace LogGuard_v0._1.AppResources.Controls.LogGWindows
 
         }
 
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e);
+        }
+
+        /// <summary>
+        /// Message detail:
+        /// https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-getminmaxinfo
+        /// </summary>
+        private const int WM_GETMINMAXINFO = 0x0024;
+
+        private System.IntPtr WindowProc(
+              System.IntPtr hwnd,
+              int msg,
+              System.IntPtr wParam,
+              System.IntPtr lParam,
+              ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_GETMINMAXINFO:
+                    WmGetMinMaxInfo(hwnd, lParam);
+                    handled = true;
+                    break;
+            }
+
+            return (System.IntPtr)0;
+        }
+
+        private void WmGetMinMaxInfo(System.IntPtr hwnd, System.IntPtr lParam)
+        {
+
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+
+            // Adjust the maximized size and position to fit the work area of the correct monitor
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            System.IntPtr monitor = NativeMethods.MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != System.IntPtr.Zero)
+            {
+
+                MONITORINFO monitorInfo = new MONITORINFO();
+                NativeMethods.GetMonitorInfo(monitor, monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+            }
+
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
     }
 }
